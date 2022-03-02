1b2a35e67ec8ab9a8ae82e4a5f42246413b6273c
TakKokubu <kokubu.tk@gmail.com>
2022-03-02 17:24:40 +0900

    book seed ファイルの作成

---
+++ db/migrate/20220302064225_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :author
+      t.date :publication_date
+      t.integer :stock
+      t.integer :price
+      t.string :image
+      
+      t.timestamps
+    end
+  end
+end
